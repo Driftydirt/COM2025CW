b4c7db3e158b8e1b9f1d51c68e9ad2c03f093f2e
Ross Treadway <ross@alantreadway.net>
2020-11-08 16:56:11 +0000

    Added basic testing

--- myapp/test/controllers/home_controller_test.rb
+++ myapp/test/controllers/home_controller_test.rb
@@ -2,8 +2,12 @@ require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get home_home_url
+    get root_url
     assert_response :success
+
+    assert_select 'title', 'Car Park Bookings'
+    assert_select 'h1', 'Car Park Bookings'
+    assert_select 'p', 'Welcome to my car park bookings site!'
   end
 
 end
