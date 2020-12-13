370268d3e7e5810260cc9e8306405f6ebf98f0af
Ross Treadway <ross@alantreadway.net>
2020-12-13 16:22:53 +0000

    Adding comments

--- myapp/app/controllers/booking_controller.rb
+++ myapp/app/controllers/booking_controller.rb
@@ -2,6 +2,7 @@ class BookingController < ApplicationController
   def new
   end 
 
+  # Create method for bookings that come from the new.html.haml
   def create
       @parking_space = ParkingSpace.find(booking_params.values.at(2))
       if check_overlap
@@ -9,8 +10,11 @@ class BookingController < ApplicationController
         return
       end
       parking_space.booking.create(booking_params)
+      redirect_to root_path
   end
 
+  # Checks all bookings in the database to see if the current space
+  # has been booked for the time in the newly created booking
   def check_overlap
     bookings = @parking_space.booking.all
     booking = booking_params.values
@@ -22,6 +26,7 @@ class BookingController < ApplicationController
     return false
   end
 
+  # Create method for the bookings the come form create_alt.haml.html
   def create_alt
       @parking_space = ParkingSpace.find(params[:id])
       if check_overlap
@@ -40,17 +45,8 @@ class BookingController < ApplicationController
     @parking_space = ParkingSpace.find(params[:id])
   end
 
-   
+  # Method for checking the values of the booking before they are saved
   def booking_params
     params.require(:booking).permit(:driver_name, :license_plate_number, :space_number, :start_time, :finish_time, :cost)
-  end
-
-  def generate_available_times(bookings)
-    available_times = Array.new(24)
-    for booking in bookings do
-      available_times.insert(booking.finish_time.to_i / 100, "booked")
-      available_times.insert(booking.start_time.to_i / 100, "booked")
-    end
-    return available_times 
   end
 end
