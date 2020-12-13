5fcf12c7e9bcb879debb4b21015ebdb8e188b887
Ross Treadway <ross@alantreadway.net>
2020-12-13 16:20:43 +0000

    Removing redundant code

--- myapp/app/controllers/car_park_controller.rb
+++ myapp/app/controllers/car_park_controller.rb
@@ -1,14 +1,6 @@
 class CarParkController < ApplicationController
-  def create
-    CarPark.create(car_park_params)
-  end
   def car_park
     @car_park = CarPark.all
   end
 
-  private
-
-  def car_park_params
-    params.require(:car_park).permit(:name, :number_spaces)
-  end
 end
--- myapp/app/helpers/bookings_helper.rb
+++ myapp/app/helpers/bookings_helper.rb
@@ -1,6 +1,2 @@
 module BookingsHelper
-
-    def get_cost(booking)
-        return (booking.finish_time.to_i-booking.start_time.to_i)/100
-    end
 end
